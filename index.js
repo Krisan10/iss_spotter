292e2019fe6014381f1b762c24ac0e2abaa5c348
Krisan Manoharan <krisan1010@hotmail.com>
2024-03-01 23:17:56 -0500

    add fetchCoordsFromIP

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const { fetchMyIP } = require('./iss');
+const { fetchMyIP, fetchCoordsFromIP } = require('./iss');
 
 fetchMyIP((error, ip) => {
   if (error) {
@@ -7,4 +7,12 @@ fetchMyIP((error, ip) => {
   }
 
   console.log('It worked! Returned IP:' , ip);
+});
+
+fetchCoordsFromIP("99.211.158.105", (error, coordinates) => {
+  if (error) {
+    console.error(`Error fetching coordinates: ${error.message}`);
+  } else {
+    console.log(`Coordinates for IP ${"99.211.158.105"}: ${coordinates.latitude}, ${coordinates.longitude}`);
+  }
 });
--- iss.js
+++ iss.js
@@ -23,9 +23,64 @@ const fetchMyIP = function(callback) {
       const ip = data.ip;
       callback(null, ip);
     } catch (parseError) {
-      callback(parseError, null);
+      callback(new Error(`Error parsing IP: ${parseError}`), null);
+    }
+  }
+  );
+};
+
+
+const fetchCoordsFromIP = function(ip, callback) {
+
+  request('http://ipwho.is/', (error, response, body) => {
+    if (error) {
+      callback(error, null);
+      return;
+    }
+    if (response.statusCode !== 200) {
+      const msg = `Status Code ${response.statusCode} when fetching IP. Response: ${body}`;
+      callback(Error(msg), null);
+      return;
+    }
+
+    if (response.statusCode !== 200) {
+      callback(new Error(`Unexpected status code: ${response.statusCode}`), null);
+      return;
+    }
+
+    try {
+      const data = JSON.parse(body);
+      const coordinates = {
+        latitude: null,
+        longitude: null,
+      };
+
+      if (data.latitude !== undefined && data.latitude !== null) {
+        coordinates.latitude = Number(data.latitude);
+      }
+
+      if (data.longitude !== undefined && data.longitude !== null) {
+        coordinates.longitude = Number(data.longitude);
+      }
+
+      callback(null, coordinates);
+    } catch (parseError) {
+      callback(new Error(`Error parsing coordinates: ${parseError}`), null);
     }
   });
 };
 
-module.exports = { fetchMyIP };
+
+fetchCoordsFromIP("99.211.158.105", (error, coordinates) => {
+  if (error) {
+    console.error(`Error fetching coordinates: ${error.message}`);
+  } else {
+    console.log(`Coordinates for IP ${"99.211.158.105"}: ${coordinates.latitude}, ${coordinates.longitude}`);
+  }
+});
+
+module.exports = {
+  fetchMyIP,
+  fetchCoordsFromIP
+
+};
